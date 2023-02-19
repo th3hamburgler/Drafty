5be6bf762c67dd2b9479ba5249ff385b18477054
Jim Wardlaw <jim.wardlaw@gmail.com>
2023-02-19 13:13:03 +0000

    Deployed c6fa8d7

--- app/adapters/league.js
+++
@@ -1,8 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default class LeagueAdapter extends ApplicationAdapter {
-  buildURL(modelName, id, snapshot, requestType, query) {
-    const url = super.buildURL(modelName, id, snapshot, requestType, query);
-    return `${url}/details`;
-  }
-}
